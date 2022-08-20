91c5c1c052c23821d943aeef4e5de88b1798636e
Jesus García <jeigar2@gmail.com>
2022-08-20 05:51:52 +0200

    Reto 1, indicar el rango de temperatura de tu ciudad imprimiendola por pantalla.

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let minimo: i8 = -10;
+    let maximo: u8 = 45;
+    let ciudad: &str = "Toledo";
+    print!("La temperatura de {} tiene el rango [{}-{}]", ciudad, minimo, maximo);
+}
