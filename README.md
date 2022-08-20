fe1dd7f45feaa88cb7a00babb33a258286f6c7c8
Jesus García <jeigar2@gmail.com>
2022-08-20 07:15:33 +0200

    Recibiendo datos del usuario por pantalla

---
+++ README.md
@@ -0,0 +1,25 @@
+# Curso Básico de Rust
+
+## Recibiendo datos del usuario
+
+Permitir la interacción del usuario con la aplicación a través de la interfaz de línea de comandos.
+
+### Inputs en Rust
+
+- Para recibir datos del usuario, se hace uso de algunas librerías internas de Rust que acceden al sistema operativo y permiten que el usuario ingrese datos por consola.
+
+- `std::io::stdin().read_line(&mut nombre).unwrap();`
+  - **std** es una librería de Rust para acceder al sistema operativo.
+  - **io** significa inputs/outputs,
+  - **stdin()** permite traer información
+  - **read_line()** indica que esa información será recibida por consola.
+  - **&mut nombre** es la variable donde guarda el dato ingresado por el usuario (Utiliza mut en las variables para indicar que la misma cambiará de valor).
+  - **unwrap()**, ayuda con el manejo de errores.
+
+### Cambiando el tipo de dato en Rust
+
+- Por defecto, los datos que ingresa el usuario por consola son del tipo String.
+- Si necesitas números enteros hay que convertir el tipo de dato
+
+- `let edad_int: u8 = edad.trim().parse().unwrap();`
+  - **parse()** hace la conversión al tipo de dato definido en la variable que se asigna
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,26 @@
 fn main() {
-    let minimo: i8 = -10;
-    let maximo: u8 = 45;
-    let ciudad: &str = "Toledo";
-    println!("La temperatura de {} tiene el rango [{}; {}]", ciudad, minimo, maximo);
+    // mensaje a mostar en pantalla
+    println!("Ingrese su nombre:"); 
+    // debe ser mutable para recibir el nombre
+    let mut nombre: String = String::new(); 
+    // unwrap se utiliza para tratar los errores
+    std::io::stdin().read_line(&mut nombre).unwrap(); 
+    // formatea el texto para eliminar saltos de línea y espacios en blanco.
+    nombre = nombre.trim().to_string(); 
+
+    println!("Que Dios te bendiga {}", nombre);
+
+    // mensaje a mostar en pantalla
+    println!("Ingrese su edad:"); 
+    // debe ser mutable para recibir el texto con la edad
+    let mut edad: String = String::new(); 
+    std::io::stdin().read_line(&mut edad).unwrap();
+    // formatea el texto para eliminar saltos de línea y espacios en blanco 
+    // y transforma a entero
+    let int_edad: u8 = edad.trim().parse().unwrap(); 
+
+    // {} es un placeholder 
+    // cada uno de ello se reemplaza por el valor de las varibles recibidas 
+    // en el resto de argumentos de la función (nombre, int_edad)
+    println!("{} tienes {} años", nombre, int_edad); 
 }
