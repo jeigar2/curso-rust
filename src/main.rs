0014d82adf1da4184277654d54ad25aa70af75eb
Jesus García <jeigar2@gmail.com>
2022-08-20 06:11:23 +0200

    se cambia el rango y el separador es un punto y coma en lugar de un guión

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ fn main() {
     let minimo: i8 = -10;
     let maximo: u8 = 45;
     let ciudad: &str = "Toledo";
-    print!("La temperatura de {} tiene el rango [{}-{}]", ciudad, minimo, maximo);
+    print!("La temperatura de {} tiene el rango [{}; {}]", ciudad, minimo, maximo);
 }
