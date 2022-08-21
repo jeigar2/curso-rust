9f8dedee207529b202b89f23fc89337924622509
Jesus García <jeigar2@gmail.com>
2022-08-21 09:05:46 +0200

    Version con popurri de opciones en comentarios

--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,111 @@
-use regex::Regex;
+use csv::{ReaderBuilder, StringRecord};
+use std::{fs, collections::HashMap};
 
-fn operar(mat_regex: Regex, mut expression: String, _sign: &str) -> String {
+const FILENAME: &str = "history.csv";
+const FIRST_TAG: &str = "INICIO";
+const SITUATION: &str = "SITUACION";
+const OPTION: &str = "OPCION";
 
-    loop {
-        let caps = mat_regex.captures(&expression);
+#[derive(Debug)]
+struct HistoryData{
+    data_type: String,
+    tag: String,
+    _text: String,
+    life: i32,
+    options: Vec<HistoryData>
+}
 
-        // si ya no queda que leer salir del loop
-        if caps.is_none() {
-            break;
-        }
+impl HistoryData {
+    fn new(row: StringRecord) -> HistoryData{
 
-        let caps = caps.unwrap();
-
-        let cap_expression = caps.get(0).unwrap().as_str();
-        let left_value: i32 = caps.get(1).unwrap().as_str().parse().unwrap();
-        let right_value: i32 = caps.get(3).unwrap().as_str().parse().unwrap();
-        let _sign = caps.get(2).unwrap().as_str();
-
-        if _sign == "*" {
-            let result = left_value * right_value;
-            expression = expression.replace(cap_expression, &result.to_string());
-        } else if _sign == "/" {
-            let result = left_value / right_value;
-            expression = expression.replace(cap_expression, &result.to_string());
-        } else if _sign == "+" {
-            let result = left_value + right_value;
-            expression = expression.replace(cap_expression, &result.to_string());
-        } else if _sign == "-" {
-            let result = left_value - right_value;
-            expression = expression.replace(cap_expression, &result.to_string());
-        }
+        let life = row.get(3).unwrap().trim();
+        let life : i32 = life.parse().unwrap_or(0);
+        return HistoryData {
+            data_type:row.get(0).unwrap().trim().to_string(),
+            tag:row.get(1).unwrap().trim().to_string(),
+            _text:row.get(2).unwrap().trim().to_string(),
+            life:life,
+            options: vec![]
+        };
     }
-    return expression;
 }
 
 fn main() {
-    // mensaje a mostar en pantalla
-    println!("Calculadora");
+    let mut life = 100;
+    let mut current_tag = FIRST_TAG;
+    let mut last_record: String = "".to_string();
+    // let mut history_datas: Vec<HistoryData> = vec![];
+    let mut history_datas: HashMap<String, HistoryData> = HashMap::new();
 
-    let mat_regex_mult = Regex::new(r"(\d+)\s?(\*)\s?(\d+)").unwrap();
-    let mat_regex_div = Regex::new(r"(\d+)\s?(/)\s?(\d+)").unwrap();
-    let mat_regex_add = Regex::new(r"(\d+)\s?(\+)\s?(\d+)").unwrap();
-    let mat_regex_sust = Regex::new(r"(\d+)\s?(-)\s?(\d+)").unwrap();    
-    // debe ser mutable para recibir la expression matemática
-    let mut expression: String = String::new();
-    // unwrap se utiliza para tratar los errores
-    std::io::stdin().read_line(&mut expression).unwrap();
+    let content: String = fs::read_to_string(FILENAME).unwrap();
+    // println!("{}", content);
 
-    expression = operar(mat_regex_mult, expression, "*");
-    expression = operar(mat_regex_div, expression, "/");
-    expression = operar(mat_regex_add, expression, "+");
-    expression = operar(mat_regex_sust, expression, "-");
+    let mut rdr = ReaderBuilder::new().delimiter(b';').from_reader(content.as_bytes());
 
+    for result in rdr.records() {
+        let result = result.unwrap();
+        let data = HistoryData::new(result);
+        let record_tag = data.tag.clone();
+        if data.data_type == SITUATION {
+            history_datas.insert(record_tag.clone(), data); // TODO el clone está repetido.
+            last_record = record_tag;
+        } else if data.data_type == OPTION {
+            if let Some(_data) = history_datas.get_mut(&last_record) {
+                (*_data).options.push(data);
+                // println!("{}",(*_data).tag);
+            }
 
-    // Mostrar resultado
-    println!("Resultado: {}", expression);
+        }
+
+        // let life = result.get(3).unwrap().trim();
+        // let life : i32 = life.parse().unwrap_or(0);
+
+        // let _data = HistoryData {
+        //     data_type:result.get(0).unwrap().trim().to_string(),
+        //     tag:result.get(1).unwrap().trim().to_string(),
+        //     _text:result.get(2).unwrap().trim().to_string(),
+        //     life:life
+        // };
+        //history_datas.push(data);
+        
+    }
+
+    // Game loop
+    loop {
+        println!("Tienes {} de vida", life);
+        if let Some(data) = history_datas.get(current_tag) {
+            println!("{}", data._text);
+            for (index, option) in data.options.iter().enumerate() {
+                println!("[{}] {}", index, option._text);
+            }
+            let mut selection = String::new();
+            std::io::stdin().read_line(&mut selection).unwrap();
+            let selection = selection.trim().parse().unwrap_or(99);
+
+            if let Some(selection_option) = &data.options.get(selection){
+                current_tag = &selection_option.tag;
+            // } else if (selection == 77) {
+            //     break;
+            } 
+            else {
+                println!("Comando no valido");
+            }
+            if data.life < 0 {
+                println!("¡Oh!, Tu salud merma");
+            }
+            life += data.life;
+            println!("");
+        } else {
+            break;
+        }
+
+        if life <= 0 {
+            println!("Se acabo el juego, no te queda vida");
+            break;
+        }
+    }
+
+    //println!("{:?}", history_datas);
+    // println!("\n-----\n{:?}\n-----\n", history_datas["DERECHA"]);
+
 }
